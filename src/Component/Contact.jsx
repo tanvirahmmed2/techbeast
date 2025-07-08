f120fb3c7a9f2848ddd05836b3c3a2955a449f9d
tanvirahmmed2 <tanvir004006@gmail.com>
2025-07-08 10:44:04 +0600

    update

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -4,20 +4,12 @@ import axios from "axios"
 
 const Contact = () => {
     UsePageTitle("Contact")
-    const [user, setUser] = useState([])
-    useEffect(() => {
-         const getallUser= async ()=>{
-           const response= await axios.get("http://localhost:3000/user")
-           setUser(response.data)
-           console.log(user);
-           
-         }
-
-    }, [])
+    
 
     return (
         <div>
 
+hello
 
 
 
